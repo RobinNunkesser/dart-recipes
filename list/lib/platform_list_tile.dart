6ea537827da83fe413e583d4b876037f8dd0c4ae
Robin Nunkesser <robin.nunkesser@hshl.de>
2018-11-28 12:35:52 +0100

    Use ListTile wrapped in Material instead of custom tile

--- list/lib/platform_list_tile.dart
+++ list/lib/platform_list_tile.dart
@@ -1,14 +1,12 @@
-import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
-class PlatformListTile extends PlatformWidgetBase<MergeSemantics, MergeSemantics> {
-
+class PlatformListTile extends PlatformWidgetBase<Material, MergeSemantics> {
   final String title;
   final String subtitle;
 
   PlatformListTile({this.title, this.subtitle});
-
 
   @override
   MergeSemantics createAndroidWidget(BuildContext context) {
@@ -22,39 +20,15 @@ class PlatformListTile extends PlatformWidgetBase<MergeSemantics, MergeSemantics
   }
 
   @override
-  MergeSemantics createIosWidget(BuildContext context) {
-    return MergeSemantics(
-        child: SafeArea(
-          top: false,
-          bottom: false,
-          child: Padding(
-            padding: const EdgeInsets.only(left: 16.0, top: 8.0, bottom: 8.0, right: 8.0),
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(title),
-                        const Padding(padding: EdgeInsets.only(top: 8.0)),
-                        Text(subtitle,
-                          style: TextStyle(
-                            color: Color(0xFF8E8E93),
-                            fontSize: 13.0,
-                            fontWeight: FontWeight.w300,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
+  Material createIosWidget(BuildContext context) {
+    return Material(
+        color: Color(0xFFFFFFFF),
+        child: MergeSemantics(
+          child: ListTile(
+            title: Text(title),
+            subtitle: Text(subtitle),
+            dense: false,
           ),
-        )//Text('$title\n$subtitle')
-    );
+        ));
   }
 }
-
