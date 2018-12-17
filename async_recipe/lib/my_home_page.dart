bf89bfcccc49a1c4873554d8ffc670862167fe84
Robin Nunkesser <robin.nunkesser@hshl.de>
2018-12-17 11:23:46 +0100

    Better use of Future

--- async_recipe/lib/my_home_page.dart
+++ async_recipe/lib/my_home_page.dart
@@ -23,12 +23,14 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void asyncMethod() async {
-    int value = await asyncMethodWithReturn();
-    print('Asynchronous return $value');
+    asyncMethodWithReturn()
+    .then((value) => print('Asynchronous return $value'))
+    .catchError((e) => print('Returned error $e'));
   }
 
   Future<int> asyncMethodWithReturn() async {
     await Future.delayed(Duration(seconds: 4),() {});
+    //return Future.error(Exception("Error message"));
     return 42;
   }
 
