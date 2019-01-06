65cf45a170df19fd9f04c05c3b7722612cb13cbb
Robin Nunkesser <robin.nunkesser@hshl.de>
2019-01-06 12:02:58 +0100

    Changed to showLicensePage

--- license/lib/my_home_page.dart
+++ license/lib/my_home_page.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
-import 'package:flutter/services.dart' show rootBundle;
 
 class MyHomePage extends StatelessWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
   final String title;
-  final Future<String> _localHTML =
-      rootBundle.loadString('assets/open_source_licenses.html');
 
   @override
   Widget build(BuildContext context) {
@@ -15,23 +11,11 @@ class MyHomePage extends StatelessWidget {
         appBar: AppBar(
           title: Text(title),
         ),
-        body: FutureBuilder<String>(
-          future: _localHTML,
-          builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-            switch (snapshot.connectionState) {
-              case ConnectionState.waiting:
-                return const CircularProgressIndicator();
-              default:
-                if (snapshot.hasError)
-                  return Text('Error: ${snapshot.error}');
-                else
-                  return WebView(
-                    initialUrl:
-                        Uri.dataFromString(snapshot.data, mimeType: 'text/html')
-                            .toString(),
-                  );
-            }
-          },
+        body: Center(
+          child: IconButton(
+            icon: Icon(Icons.info),
+            onPressed: () => showLicensePage(context: context),
+          ),
         ));
   }
 }
