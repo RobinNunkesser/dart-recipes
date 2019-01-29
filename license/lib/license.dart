fb0ca9735a56f88284020d7425115610fed8b708
Robin Nunkesser <robin.nunkesser@hshl.de>
2019-01-29 18:08:01 +0100

    Refactoring

---
+++ license/lib/license.dart
@@ -0,0 +1,2 @@
+export 'package:license/my_app.dart';
+export 'package:license/my_home_page.dart';
--- license/lib/main.dart
+++ license/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'my_app.dart';
+import 'package:license/license.dart';
 
 void main() => runApp(MyApp());
 
