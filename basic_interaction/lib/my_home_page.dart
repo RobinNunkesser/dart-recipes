47e20e061673b642e9a4b6733a1c6fd2d55855c5
Robin Nunkesser <robin.nunkesser@hshl.de>
2020-03-03 17:35:28 +0100

    New recipe: Basic Interaction

---
+++ basic_interaction/lib/my_home_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var _textEditingController = TextEditingController();
+  var output = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Basic Interaction"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(
+              controller: _textEditingController,
+              decoration: InputDecoration(
+                hintText: "Enter text",
+                hintStyle: TextStyle(color: Colors.grey[300]),
+              ),
+              textAlign: TextAlign.center,
+            ),
+            MaterialButton(child: Text("Process"), onPressed: _process),
+            Text(output),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _process() {
+    setState(() {
+      output = _textEditingController.text.toUpperCase();
+    });
+  }
+}
