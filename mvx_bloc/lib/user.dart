ce9d846a707390e32773ba5bd9820c6a9260aebd
Robin Nunkesser <robin.nunkesser@hshl.de>
2022-02-04 12:06:19 +0100

    Bugfix

--- mvx_bloc/lib/user.dart
+++ mvx_bloc/lib/user.dart
@@ -1,36 +1,37 @@
 import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
 
-class User {
-  String forename = '';
-  String surname = '';
+class User extends Equatable {
+  final String forename;
+  final String surname;
   String get greeting => 'Hello $forename!';
 
-  User({String forename = '', String surname = ''});
+  const User({this.forename = '', this.surname = ''});
 
   User copyWith({
     String? forename,
     String? surname,
-  }) {
-    return User(
-        forename: forename ?? this.forename, surname: surname ?? this.surname);
-  }
+  }) =>
+      User(
+          forename: forename ?? this.forename,
+          surname: surname ?? this.surname);
+
+  @override
+  List<Object?> get props => [forename, surname];
 }
 
 class UserCubit extends Cubit<User> {
-  UserCubit() : super(User());
+  UserCubit() : super(const User());
 
   set forename(String newValue) {
     if (state.forename != newValue) {
       emit(state.copyWith(forename: newValue));
-      state.forename = newValue;
-      emit(state);
     }
   }
 
   set surname(String newValue) {
     if (state.surname != newValue) {
       emit(state.copyWith(surname: newValue));
-      state.surname = newValue;
     }
   }
 }
