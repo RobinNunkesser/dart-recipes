8a8e5be27190f8af601b8703375a3a67c81aeccf
Robin Nunkesser <robin.nunkesser@hshl.de>
2022-02-08 17:41:19 +0100

    New recipe

---
+++ video_recipe/android/app/src/main/kotlin/com/example/video_recipe/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.video_recipe
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ video_recipe/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:video_recipe/video_app.dart';
+
+void main() {
+  runApp(VideoApp());
+}
